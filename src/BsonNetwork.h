053491021b4cef8be8a7dea52ae6c0e68f258ea3
jbenet <jbenet@cs.stanford.edu>
2010-11-28 02:14:48 -0800

    Added debug log

--- src/BsonNetwork.h
+++ src/BsonNetwork.h
@@ -2,3 +2,16 @@
 
 #import "BNConnection.h"
 #import "BNServer.h"
+
+// #ifdef DEBUG
+#define BSONNETWORK_DEBUG
+// #endif
+
+#ifndef DebugLog
+  #ifdef BSONNETWORK_DEBUG
+    #define DLog(fmt, ...) NSLog((@"%s [line %d] " fmt), \
+              __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+  #else
+    #define DLog(...)
+  #endif
+#endif
